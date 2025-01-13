d1bcd47bf054dcfc1818b6db564db555c4e37b84
Sjur Groven <sjurgr@stud.ntnu.no>
2025-01-13 02:31:27 +0100

    c-kode oppg3

--- shared variable/c/foo.c
+++ shared variable/c/foo.c
@@ -9,11 +9,17 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for(int j=0;j<1000000;j++){
+        i+=1;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for(int j=0;j<1000000;j++){
+        i-=1;
+    }
     return NULL;
 }
 
@@ -22,11 +28,14 @@ int main(){
     // TODO: 
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
-    
+    pthread_t thread1, thread2;
+    pthread_create(&thread1,NULL,incrementingThreadFunction,NULL);
+    pthread_create(&thread1,NULL,decrementingThreadFunction,NULL);
     // TODO:
     // wait for the two threads to be done before printing the final result
     // Hint: Use `pthread_join`    
-    
+    pthread_join(thread1,NULL);
+    pthread_join(thread2,NULL);
     printf("The magic number is: %d\n", i);
     return 0;
 }
