2c14f757b91aeff920a40b5bb1a01df216604e06
Sjur Groven <sjurgr@stud.ntnu.no>
2025-01-16 16:31:02 +0100

    oppg4 med for / select implementasjon i Go

--- shared variable/go/foo.go
+++ shared variable/go/foo.go
@@ -15,29 +15,44 @@ type SafeCounter struct {
 
 var i = 0
 
-func (c *SafeCounter) incrementing() {
+func incrementing(channel chan int) {
 	//TODO: increment i 1000000 times
-	for j := 0; j < 1000000; j++ {
-		c.mu.Lock()
-		i += 1
-		c.mu.Unlock()
+	for j := 0; j < 1e6; j++ {
+		channel <- 1
 	}
+	close(channel)
 }
-func (c *SafeCounter) decrementing() {
+func decrementing(channel chan int) {
 	//TODO: decrement i 1000000 times
-	for j := 0; j < 1000000; j++ {
-		c.mu.Lock()
-		i -= 1
-		c.mu.Unlock()
+	for j := 0; j < 1e6; j++ {
+		channel <- 1
 	}
+	close(channel)
 }
 
 func main() {
 	// What does GOMAXPROCS do? What happens if you set it to 1?
 	runtime.GOMAXPROCS(2)
-	c := SafeCounter{}
-	go c.incrementing()
-	go c.decrementing()
+	chan1 := make(chan int)
+	chan2 := make(chan int)
+	go incrementing(chan1)
+	go decrementing(chan2)
+	for chan1 != nil || chan2 != nil {
+		select {
+		case incr, ok := <-chan1:
+			if !ok {
+				chan1 = nil
+			} else if incr == 1 {
+				i += 1
+			}
+		case decr, ok := <-chan2:
+			if !ok {
+				chan2 = nil
+			} else if decr == 1 {
+				i -= 1
+			}
+		}
+	}
 	// TODO: Spawn both functions as goroutines
 	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
 	// We will do it properly with channels soon. For now: Sleep.
